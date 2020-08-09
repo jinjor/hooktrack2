90f55265ecef05da33e0004ce531ec90717e7b31
jinjor <jinjorweb@gmail.com>
2020-08-09 17:41:58 +0900

    fix JS

--- public/script.js
+++ public/script.js
@@ -1,19 +1,21 @@
 console.log("hello from JS");
 
-const res = await fetch("/api/endpoints", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ method: "GET", response: { body: "Hello!" } }),
-});
-const json = await res.json();
-console.log(json);
+(async () => {
+  const res = await fetch("/api/endpoints", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ method: "GET", response: { body: "Hello!" } }),
+  });
+  const json = await res.json();
+  console.log(json);
 
-const res1 = await fetch(`/api/${json.key}`);
-const json1 = await res1.json();
-console.log(json1);
+  const res1 = await fetch(`/api/${json.key}`);
+  const text1 = await res1.text();
+  console.log(text1);
 
-const res2 = await fetch(`/api/endpoints/${json.key}/results`);
-const json2 = await res2.json();
-console.log(json2);
+  const res2 = await fetch(`/api/endpoints/${json.key}/results`);
+  const json2 = await res2.json();
+  console.log(json2);
+})();
