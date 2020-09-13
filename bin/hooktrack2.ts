50f82df6aa580628ee70172c0df37672d219e181
jinjor <jinjorweb@gmail.com>
2020-09-14 01:01:24 +0900

    fix stack name

--- bin/hooktrack2.ts
+++ bin/hooktrack2.ts
@@ -4,7 +4,7 @@ import * as cdk from "@aws-cdk/core";
 import { Hooktrack2Stack } from "../lib/hooktrack2-stack";
 
 const app = new cdk.App();
-new Hooktrack2Stack(app, "Hooktrack2Stack-with-front", {
+new Hooktrack2Stack(app, "Hooktrack2Stack", {
   // env: {
   //   account: process.env.CDK_DEFAULT_ACCOUNT,
   //   region: process.env.CDK_DEFAULT_REGION,
